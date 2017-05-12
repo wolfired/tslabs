3782268f618247b583bb08ad8b4a35cd700dc303
LinkWu <wjianl@qq.com>
2017-05-12 21:26:16 +0800

    数据与工具函数

---
+++ src/utils.ts
@@ -0,0 +1,7 @@
+export function Sleep(ms: uint) {
+    return new Promise<void>((resolve, reject) => {
+        if (0 === setTimeout(() => { resolve(); }, ms)) {
+            reject("error");
+        }
+    });
+}
