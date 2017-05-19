a395ab98d8bc14b4bfa5ca7e21bde51b74652b5e
LinkWu <wjianl@qq.com>
2017-05-19 20:26:21 +0800

    .obj文件解析

--- src/parse/obj.ts
+++ src/parse/obj.ts
@@ -1,59 +1,228 @@
 import { IParser } from "../parse";
+import * as utf8 from "../unicode/utf8";
 
-const minus: uint = 45;// -
-const point: uint = 46;// .
-const zero: uint = 48;// 0
-const pound: uint = 35;// #
-const cr: uint = 13;// \r
-const lf: uint = 10;// \n
-const space: uint = 32// space
+const minus: byte = 45;// -
+const point: byte = 46;// .
+const zero: byte = 48;// 0
+const pound: byte = 35;// #
+const cr: byte = 13;// \r
+const lf: byte = 10;// \n
+const tab: byte = 9;// tab
+const space: byte = 32// space
+const slash: byte = 47// /
 
-function comment_handler(buffer: ArrayBuffer): void {
-    let raw = new Uint8Array(buffer);
 
-}
+/**
+ * 判断当前位置是不是行尾，返回行尾字节数
+ */
+function isLineEnd(buffer: Uint8Array, begin: int): [bool, int] {
+    const len: int = buffer.byteLength;
 
-function string_handler(): void {
-
-}
-
-function number_handler(buffer: ArrayBuffer): float32 {
-    let raw = new Uint8Array(buffer);
-
-    let sign: float32 = 1.0;
-    let sum: float32 = 0.0;
-    let divisor: float32 = 0.0;
-    for (var index = 0; index < raw.byteLength; ++index) {
-        let v: uint8 = raw[index];
-
-        if (minus === v) {
-            sign = -1.0;
-            continue;
-        } else if (point === v) {
-            divisor = 1.0;
-            continue;
-        }
-
-        sum = sum * 10 + v - 48;
-
-        if (0.0 < divisor) {
-            divisor *= 10.0;
-        }
+    if (1 < (len - begin) && cr === buffer[begin] && lf === buffer[begin + 1]) {
+        return [true, 2];
     }
 
-    return 0.0 === divisor ? sign * sum : sign * sum / divisor;
+    if (cr === buffer[begin] || lf === buffer[begin]) {
+        return [true, 1];
+    }
+
+    return [false, 1];
+}
+
+/**
+ * 判断当前位置是不是词尾，返回词尾字节数
+ */
+function isWordEnd(buffer: Uint8Array, begin: int): [bool, int] {
+    const end: int = buffer.byteLength;
+
+    if (space === buffer[begin]) {
+        return [true, 1];
+    }
+
+    return isLineEnd(buffer, begin);
+}
+
+/**
+ * 返回下一行的开始位置
+ */
+function cutLine(buffer: Uint8Array, begin: int): int {
+    while (begin < buffer.byteLength) {
+        let [e, s] = isLineEnd(buffer, begin);
+        begin += s;
+        if (e) {
+            break;
+        }
+    }
+    return begin;
+}
+
+/**
+ * 返回下一词的开始位置
+ */
+function cutWord(buffer: Uint8Array, begin: int): int {
+    while (begin < buffer.byteLength) {
+        let [e, s] = isWordEnd(buffer, begin);
+        begin += s;
+        if (e) {
+            break;
+        }
+    }
+    return begin;
+}
+
+function dumpWord(buffer: Uint8Array, begin: int, end: int): string {
+    for (let i = begin; i < end; ++i) {
+        if (cr !== buffer[i] && lf !== buffer[i] && tab !== buffer[i]) {
+            begin = i;
+            break;
+        }
+    }
+    for (let i = end - 1; i >= begin; --i) {
+        if (cr !== buffer[i] && lf !== buffer[i] && tab !== buffer[i]) {
+            end = i;
+            break;
+        }
+    }
+    return String.fromCodePoint(...buffer.slice(begin, end));
+}
+
+function dumpNumber(buffer: Uint8Array, begin: int, end: int): float32 {
+    return parseFloat(dumpWord(buffer, begin, end));
 }
 
 export class Obj implements IParser {
-    public readonly vertex_coord: Uint8Array;
-    public readonly texture_coord: Uint8Array;
-    public readonly normal_vector: Uint8Array;
+    public readonly vs: VertexData = null!;
+    public readonly vts: VertexData = null!;
+    public readonly vns: VertexData = null!;
+    public readonly fs: TriangleData = null!;
 
     public parse(raw: Uint8Array): void {
-        let begin: uint = 0;
-        let v: uint8;
+        const vs: float32[] = [];
+        const vns: float32[] = [];
+        const fs: uint32[] = [];
+
+        let line_begin: int = 0;
+        let line_end: int = 0;
         while (true) {
-            v = raw[begin];
+            line_end = cutLine(raw, line_begin);
+
+            if (pound !== raw[line_begin]) {
+                while (line_begin < line_end) {
+                    let word_begin = line_begin;
+                    let word_end: int;
+                    let word: string;
+                    let nums: string[];
+                    let num: int;
+
+
+                    word_end = cutWord(raw, word_begin);
+                    word = dumpWord(raw, word_begin, word_end);
+                    word_begin = word_end;
+
+                    switch (word) {
+                        case "v":
+                            word_end = cutWord(raw, word_begin);
+                            vs.push(dumpNumber(raw, word_begin, word_end));
+                            word_begin = word_end;
+
+                            word_end = cutWord(raw, word_begin);
+                            vs.push(dumpNumber(raw, word_begin, word_end));
+                            word_begin = word_end;
+
+                            word_end = cutWord(raw, word_begin);
+                            vs.push(dumpNumber(raw, word_begin, word_end));
+                            word_begin = word_end;
+                            break;
+                        case "vn":
+                            word_end = cutWord(raw, word_begin);
+                            vns.push(dumpNumber(raw, word_begin, word_end));
+                            word_begin = word_end;
+
+                            word_end = cutWord(raw, word_begin);
+                            vns.push(dumpNumber(raw, word_begin, word_end));
+                            word_begin = word_end;
+
+                            word_end = cutWord(raw, word_begin);
+                            vns.push(dumpNumber(raw, word_begin, word_end));
+                            word_begin = word_end;
+                            break;
+                        case "f":
+                            word_end = cutWord(raw, word_begin);
+                            word = dumpWord(raw, word_begin, word_end);
+                            word_begin = word_end;
+
+                            nums = word.split(/\//);
+                            fs.push(parseInt(nums[0]));
+                            fs.push(-1);
+                            fs.push(parseInt(nums[2]));
+
+                            word_end = cutWord(raw, word_begin);
+                            word = dumpWord(raw, word_begin, word_end);
+                            word_begin = word_end;
+
+                            nums = word.split(/\//);
+                            fs.push(parseInt(nums[0]));
+                            fs.push(-1);
+                            fs.push(parseInt(nums[2]));
+
+                            word_end = cutWord(raw, word_begin);
+                            word = dumpWord(raw, word_begin, word_end);
+                            word_begin = word_end;
+
+                            nums = word.split(/\//);
+                            fs.push(parseInt(nums[0]));
+                            fs.push(-1);
+                            fs.push(parseInt(nums[2]));
+                            break;
+                        default:
+                            break;
+                    }
+
+                    line_begin = word_end;
+                }
+            }
+
+            line_begin = line_end;
+
+            if (raw.byteLength <= line_begin) {
+                break;
+            }
         }
+        (<{ vs: VertexData }>this).vs = new VertexData(vs.length, 3);
+        this.vs.raw.set(vs);
+
+        (<{ vns: VertexData }>this).vns = new VertexData(vns.length, 3);
+        this.vns.raw.set(vns);
+
+        (<{ fs: TriangleData }>this).fs = new TriangleData(fs.length, 9);
+        this.fs.raw.set(fs);
+    }
+}
+
+class VertexData {
+    public readonly size: int = 0;
+    public readonly raw: Float32Array = null!;
+
+    public constructor(len: int, size: int) {
+        this.size = size;
+        this.raw = new Float32Array(len);
+    }
+
+    public get count(): int {
+        return this.raw.length / this.size;
+    }
+}
+
+class TriangleData {
+    public readonly size: int = 0;
+    public readonly raw: Int32Array = null!;
+
+    public constructor(len: int, size: int) {
+        this.size = size;
+        this.raw = new Int32Array(len);
+    }
+
+    public get count(): int {
+        return this.raw.length / this.size;
     }
 }
