4d5319bd757623c19cf1e0af4afaef960c4b4e24
LinkWu <wjianl@qq.com>
2017-06-16 19:17:03 +0800

    支持WebAssembly

--- src/builtin.d.ts
+++ src/builtin.d.ts
@@ -13,3 +13,33 @@ declare type byte = uint8;
 declare type uint = uint32;
 declare type float32 = number;
 declare type float64 = number;
+
+declare namespace WebAssembly {
+    type ImportObject = { env?: { memoryBase?: uint, tableBase?: uint, memory?: WebAssembly.Memory, table?: WebAssembly.Table } };
+
+    class Module {
+        public constructor(bytes: ArrayBufferView | ArrayBuffer);
+
+        public static exports(moduleObject: Module): Array<{ name: string, kink: string }>;
+        public static imports(moduleObject: Module): Array<{ module: string, name: string, kink: string }>;
+        public static customSections(moduleObject: Module, sectionName: string): Array<any>;
+    }
+    class Instance {
+        public constructor(moduleObject: Module, importObject?: ImportObject);
+
+        public exports: any;
+    }
+    class Memory {
+        public constructor(memoryDescriptor: { initial: uint32, maximum?: uint32 });
+    }
+    class Table {
+        public constructor(tableDescriptor: { element: "anyfunc", initial: uint32, maximum?: uint32 });
+    }
+    function validate(bytes: ArrayBufferView | ArrayBuffer): bool;
+    function compile(bytes: ArrayBufferView | ArrayBuffer): Promise<Module>;
+    type WebAssemblyInstantiatedSource = {
+        module: Module;
+        instance: Instance;
+    };
+    function instantiate(bytes: ArrayBufferView | ArrayBuffer, importObject?: ImportObject): Promise<WebAssemblyInstantiatedSource>;
+}
