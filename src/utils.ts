58bdb030534b552720620c432127d099fdbd27fc
LinkWu <wjianl@qq.com>
2017-08-05 21:03:57 +0800

    修复编译问题

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
-export function Sleep(ms: uint) {
-    return new Promise<void>((resolve, reject) => {
-        if (0 === setTimeout(() => { resolve(); }, ms)) {
-            reject("error");
-        }
-    });
-}
+// export function Sleep(ms: uint) {
+//     return new Promise<void>((resolve, reject) => {
+//         if (0 === setTimeout(() => { resolve(); }, ms)) {
+//             reject("error");
+//         }
+//     });
+// }
